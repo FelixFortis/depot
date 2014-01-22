a0d1414f4353d61c2c6548c95fc37bbe8c5db507
Adam Wenham <adamwenham64@gmail.com>
2014-01-22 21:07:07 +0000

    finished sidebar clock

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,16 +2,12 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @counter = increment_counter
-    @date = current_date
     @time = current_time
   end
 
-  def current_date
-    Date.today
-  end
-
   def current_time
-    Time.now
+    t = Time.now
+    t.strftime("%H:%M on %A %d/%m/%y")
   end
 
   def increment_counter
