74738471cc148e02020786aeced1dfd008dcaa52
Adam Wenham <adamwenham64@gmail.com>
2014-01-22 09:47:55 +0000

    added page views counter to index page and store controller index action

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,10 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
-    if session[:counter].nil?
-      session[:counter] = 1
-    else
-      session[:counter] += 1
-    end
+    @counter = increment_counter
+  end
+
+  def increment_counter
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
