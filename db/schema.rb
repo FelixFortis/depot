cef06478548d435aa8f3402f686a5072d480b7ff
Adam Wenham <adamwenham64@gmail.com>
2014-02-05 07:42:04 +0000

    migrated smart cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140124101413) do
+ActiveRecord::Schema.define(:version => 20140125210245) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
