7e00530ae62c3bff16c89e5107066dbdbc14435b
Adam Wenham <adamwenham64@gmail.com>
2014-02-11 06:40:34 +0000

    began adding functionality to add price on line_item creation

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,10 +12,11 @@ class Cart < ActiveRecord::Base
 
   def add_product(product_id)
     current_item = line_items.find_by_product_id(product_id)
+    current_price = Product.find(product_id).price
     if current_item
       current_item.quantity += 1
     else
-      current_item = line_items.build(product_id: product_id)
+      current_item = line_items.build(product_id: product_id, price: current_price)
     end
     current_item
   end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -13,7 +13,7 @@
 class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
-  attr_accessible :cart_id, :product_id
+  attr_accessible :cart_id, :product_id, :price
 
   def total_price
     product.price * quantity
