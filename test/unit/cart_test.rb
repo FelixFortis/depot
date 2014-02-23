73074bf5d2f3d87c038556f381dddf5d46b004b5
Adam Wenham <adamwenham64@gmail.com>
2014-02-23 10:12:19 +0000

    made progress in getting new tests to pass

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -10,30 +10,31 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  def new_cart_with_one_product(product_name)
-    cart = Cart.create
-    cart.add_product(products(product_name).id)
-    cart
-  end
 
   test "cart should create a new line item when adding a new product" do
     # set up cart with a product in
-    cart = new_cart_with_one_product(:one)
+    cart = Cart.new
+    cart.add_product(products(:coffee).id)
+    cart.save
     assert_equal(1, cart.line_items.count)
 
     # add a new unique product to the cart
     cart.add_product(products(:ruby).id)
+    cart.save
     assert_equal(2, cart.line_items.count)
   end
 
   test "cart should update existing line item quantity when adding an existing product" do
     # set up cart with a product in
-    cart = new_cart_with_one_product(:ruby)
+    cart = Cart.new
+    cart.add_product(products(:ruby).id)
+    cart.save
     assert_equal(1, cart.line_items.count)
 
     # add an existing product to the cart
     cart.add_product(products(:ruby).id)
+    cart.save
     assert_equal(1, cart.line_items.count)
-    assert_equal(2, cart.line_items(:ruby).quantity)
+    assert_equal(2, cart.line_items(:ruby).size)
   end
 end
