0efb10e30da6da48cff2c0373e8c3297eb9529c8
Adam Wenham <adamwenham64@gmail.com>
2014-03-26 07:14:15 +0000

    finished AJAXing the dynamic sidebar cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -49,6 +49,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
+        format.js
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
@@ -65,7 +66,6 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.update_attributes(params[:line_item])
         format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.js
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
