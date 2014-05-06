e70ad7f1ef9c5668984abdadcce5ce1c982bd450
Adam Wenham <adamwenham64@gmail.com>
2014-05-06 07:39:56 +0100

    updated order create action and order-line_item db relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
   attr_accessible :address, :email, :name, :pay_type
 
   PAYMENT_TYPES = [ "Cheque", "Credit card", "Purchase order" ]
